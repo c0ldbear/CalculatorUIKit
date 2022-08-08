c6314fbea2e288b48787a2d3378f9f31dcf18296
teddy <teddy.juhlin-henricson@bontouch.com>
2022-08-08 11:38:21 +0200

    playing around with UIButton

--- CalculatorUIKit/ViewController.swift
+++ CalculatorUIKit/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var showNumbersLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let button = UIButton(frame: CGRect(x: 100,
+                                            y: 100,
+                                            width: 200,
+                                            height: 60))
+        button.setTitle("Test", for: .normal)
+        button.setTitleColor(.systemCyan, for: .normal)
+        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        self.view.addSubview(button)
+        
     }
-
-
+    
+    @objc
+    func buttonAction() {
+        print("Button pressed!")
+        showNumbersLabel.text = "LOL"
+    }
+    
 }
 
