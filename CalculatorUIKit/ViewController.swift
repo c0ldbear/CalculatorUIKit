9a27300e9a7135621fa20f5c2f36eda9f03cc19e
teddy <teddy.juhlin-henricson@bontouch.com>
2022-08-08 15:15:20 +0200

    Add more buttons, increase label, random colors to buttons

--- CalculatorUIKit/ViewController.swift
+++ CalculatorUIKit/ViewController.swift
@@ -11,25 +11,60 @@ class ViewController: UIViewController {
     
     @IBOutlet var showNumbersLabel: UILabel!
     @IBOutlet var testButton: UIButton!
+    @IBOutlet var testButton2: UIButton!
+    @IBOutlet var testButton3: UIButton!
+    @IBOutlet var testButton4: UIButton!
+    @IBOutlet var testButton5: UIButton!
+    @IBOutlet var testButton6: UIButton!
+    @IBOutlet var testButton7: UIButton!
+    @IBOutlet var testButton8: UIButton!
+    @IBOutlet var testButton9: UIButton!
     
-    private var pressed: Bool = false
+    var firstTime: Bool = true
+    var buttonColors: [UIColor] = [ .systemCyan, .systemMint, .systemPink, .systemTeal, .systemIndigo ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        setupTestButton()
+        
+        setupNumbersLabel()
+        
+        let buttons: [UIButton?] = [testButton, testButton2, testButton3, testButton4, testButton5, testButton6, testButton7, testButton8, testButton9]
+        let titles: [String?] = ["LåL", "LöL", "LäL", "LøL", "LæL", "LaaL", "Que", "LooL", "LaoL"]
+        
+        for (button, title) in zip(buttons, titles) {
+            setupTestButton(for: button, withTitle: title)
+        }
     }
     
     @objc
-    func buttonAction() {
-        pressed = !pressed
-        showNumbersLabel.text = pressed ? "LOL" : "Label"
+    func buttonAction(for button: UIButton?) {
+        guard let button = button else {
+            return
+        }
+        
+        button.configuration?.background.backgroundColor = buttonColors.shuffled().first
+        
+        if firstTime {
+            firstTime = false
+            showNumbersLabel.text = button.currentTitle
+            return
+        }
+        showNumbersLabel.text! += button.currentTitle ?? ""
+        
     }
     
-    func setupTestButton() {
-        testButton.setTitle("test", for: .normal)
-        testButton.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+    func setupTestButton(for button: UIButton?, withTitle title: String?) {
+        guard let button = button else {
+            return
+        }
+        
+        button.setTitle(title, for: .normal)
+        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
     }
     
+    func setupNumbersLabel() {
+        showNumbersLabel.text = "LOLs will be shown here\n\nlol"
+        showNumbersLabel.numberOfLines = 10
+    }
 }
-
