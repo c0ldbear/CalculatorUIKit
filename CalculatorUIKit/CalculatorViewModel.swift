be11295599ba92e82929ee625d254f772ce20f22
teddy <teddy.juhlin-henricson@bontouch.com>
2022-08-08 15:19:39 +0200

    Change name of files and add a View Model

---
+++ CalculatorUIKit/CalculatorViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  CalculatorViewModel.swift
+//  CalculatorUIKit
+//
+//  Created by teddy juhlin-henricson on 2022-08-08.
+//
+
+import Foundation
+
+final class CalculatorViewModel {
+    
+}
