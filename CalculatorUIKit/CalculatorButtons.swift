bd4a0f0de347f8d2ca0161af251c516ee0e1faf7
teddy <teddy.juhlin-henricson@bontouch.com>
2022-08-09 09:41:58 +0200

    Add new enum for Calculator Buttons

---
+++ CalculatorUIKit/CalculatorButtons.swift
@@ -0,0 +1,29 @@
+//
+//  CalculatorButtons.swift
+//  CalculatorUIKit
+//
+//  Created by teddy juhlin-henricson on 2022-08-09.
+//
+
+import Foundation
+
+enum CalculatorButtons: String {
+    case zero = "0"
+    case one = "1"
+    case two = "2"
+    case three = "3"
+    case four = "4"
+    case five = "5"
+    case six = "6"
+    case seven = "7"
+    case eight = "8"
+    case nine = "9"
+    case clear = "c"
+    case addition = "+"
+    case subtraction = "-"
+    case multiplication = "*"
+    case division = "÷"
+    case percentage = "%"
+    case root = "√"
+    case equal = "="
+}
