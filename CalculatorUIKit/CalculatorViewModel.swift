d31a3e4384e0093372c31b7927acb555f962ca9d
teddy <teddy.juhlin-henricson@bontouch.com>
2022-08-10 14:21:23 +0200

    Move property 'allButtons' to CalculatorModel & remove string fcn 4 num

--- CalculatorUIKit/CalculatorViewModel.swift
+++ CalculatorUIKit/CalculatorViewModel.swift
@@ -14,12 +14,6 @@ final class CalculatorViewModel: ObservableObject {
         case operation(CalculatorModel.Operation)
         case number(CalculatorModel.Number)
         
-        static let allButtons: [[CalculatorButton]] = [[ .operation(.clear), .operation(.percentage), .operation(.division)],
-                                                       [.number(.seven), .number(.eight), .number(.nine), .operation(.multiplication)],
-                                                       [.number(.four), .number(.five), .number(.six), .operation(.subtraction)],
-                                                       [.number(.one), .number(.two), .number(.three), .operation(.addition)],
-                                                       [.number(.zero), .number(.punct), .operation(.equal)]]
-        
         var string: String {
             switch self {
             case .operation(let operation):
@@ -49,38 +43,15 @@ final class CalculatorViewModel: ObservableObject {
                 return ""
             }
         }
-        
-        func stringFromNumber(_ number: CalculatorModel.Number) -> String  {
-            switch number {
-            case .zero:
-                return "0"
-            case .one:
-                return "1"
-            case .two:
-                return "2"
-            case .three:
-                return "3"
-            case .four:
-                return "4"
-            case .five:
-                return "5"
-            case .six:
-                return "6"
-            case .seven:
-                return "7"
-            case .eight:
-                return "8"
-            case .nine:
-                return "9"
-            case .punct:
-                return "."
-            case .none:
-                return ""
-            }
-        }
     }
         
     @Published private(set) var calculatorModel = CalculatorModel()
+    
+    static let allButtons: [[CalculatorButton]] = [[ .operation(.clear), .operation(.percentage), .operation(.division)],
+                                                   [.number(.seven), .number(.eight), .number(.nine), .operation(.multiplication)],
+                                                   [.number(.four), .number(.five), .number(.six), .operation(.subtraction)],
+                                                   [.number(.one), .number(.two), .number(.three), .operation(.addition)],
+                                                   [.number(.zero), .number(.punct), .operation(.equal)]]
     
     init() {}
     
